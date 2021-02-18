55b94fc43d075e3e6b0f8636d828e9724cd308aa
Piotr Wałoszek <piotrek.waloszek@gmail.com>
2021-02-18 12:27:41 +0100

    Add authors JS

--- js/script.js
+++ js/script.js
@@ -46,7 +46,6 @@ function generateTitleLinks() {
     link.addEventListener('click', titleClickHandler);
   }
 }
-
 generateTitleLinks();
 
 function generateTags(customSelector = ''){
@@ -67,15 +66,13 @@ function generateTags(customSelector = ''){
     taglist.innerHTML = html;
   }
 }
-
 generateTags();
 
 function tagClickHandler(event){
-  console.log(tagClickHandler);
   event.preventDefault();
   const clickedElement = this,
     href = clickedElement.getAttribute('href'),
-    tag = href.replace('&#tag-', ''),
+    tag = href.replace('#tag-', ''),
     activeTags = document.querySelectorAll('a.active[href^="#tag-"]');
 
   for (let activeTag of activeTags){
@@ -99,3 +96,51 @@ function addClickListenersToTags(){
   }
 }
 addClickListenersToTags();
+
+function generateAuthors(){
+  const optArticleSelector = '.post',
+    optArticleAuthorSelector ='.post-author',
+    articles = document.querySelectorAll(optArticleSelector);
+
+
+  for (let article of articles){
+    const authorList = article.querySelector(optArticleAuthorSelector);
+    let html = '';
+    const articleAuthor = article.getAttribute('data-author');
+
+    for (let author of articleAuthor){
+      const authorHTML = '<a href="#author-' + articleAuthor+'"><span>' + articleAuthor + '</span></a>';
+      html = authorHTML;
+
+    }
+    authorList.innerHTML = html;
+  }
+}
+generateAuthors();
+
+function authorClickHandler(event){
+  event.preventDefault();
+  const clickedElement = this,
+    href = clickedElement.getAttribute('href'),
+    author = href.replace('#author-', ''),
+    activeAuthors = document.querySelectorAll('a.active[href^="#author-:]');
+
+  for (let activeAuthor of activeAuthors){
+    activeAuthor.classList.remove('active');
+  }
+  const clickedAuthors = document.querySelectorAll('a[href="' + href + '"]')
+
+  for (let clickedAuthor of clickedAuthors){
+    clickedAuthor.classList.add('active');
+  }
+  generateTitleLinks('[data-author~="' + author + '"]');
+}
+
+function addClickListenersToAuthors(){
+  const authors = document.querySelectorAll('a[href^="#author-"]');
+
+  for (let author of authors){
+    author.addEventListener('click', authorClickHandler);
+  }
+}
+addClickListenersToAuthors();
