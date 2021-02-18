02e27d85ebedd6985a3677c725ba9d126938f971
Piotr Wałoszek <piotrek.waloszek@gmail.com>
2021-02-18 10:38:13 +0100

    Add tagClickHandler function

--- js/script.js
+++ js/script.js
@@ -25,9 +25,7 @@ function generateTitleLinks() {
   const optArticleSelector = '.post',
     optTitleSelector = '.post-title',
     optTitleListSelector = '.titles',
-    optArticleTagSelector ='.post-tags .list';
-
-  const titleList = document.querySelector(optTitleListSelector);
+    titleList = document.querySelector(optTitleListSelector);
   titleList.innerHTML = '';
 
   const articles = document.querySelectorAll(optArticleSelector);
@@ -51,24 +49,53 @@ function generateTitleLinks() {
 
 generateTitleLinks();
 
-function generateTags(){
+function generateTags(customSelector = ''){
   const optArticleSelector = '.post',
     optArticleTagSelector ='.post-tags .list',
     articles = document.querySelectorAll(optArticleSelector);
 
-  for(let article of articles) {
-    const taglist = article.querySelector(optArticleTagSelector);
+  for (let article of articles){
+    const taglist = article.querySelector(optArticleTagSelector + customSelector);
     let html = '';
     const articleTags = article.getAttribute('data-tags');
     const articleTagsArray = articleTags.split(' ');
 
     for (let tag of articleTagsArray){
       const tagHTML = '<li><a href="#tag-' + tag + '"><span>' + tag + '</span></a></li>';
-      console.log(tagHTML);
-      html = tagHTML;
+      html = html + tagHTML;
     }
     taglist.innerHTML = html;
   }
 }
 
 generateTags();
+
+function tagClickHandler(event){
+  console.log(tagClickHandler);
+  event.preventDefault();
+  const clickedElement = this,
+    href = clickedElement.getAttribute('href'),
+    tag = href.replace('&#tag-', ''),
+    activeTags = document.querySelectorAll('a.active[href^="#tag-"]');
+
+  for (let activeTag of activeTags){
+    activeTag.classList.remove('active');
+  }
+
+  const clickedTags = document.querySelectorAll('a[href="' + href + '"]');
+
+  for (let clickedTag of clickedTags){
+    clickedTag.classList.add('active');
+  }
+
+  generateTitleLinks('[data-tags~="' + tag + '"]');
+}
+
+function addClickListenersToTags(){
+  const tags = document.querySelectorAll('a[href^="#tag-"]');
+
+  for (let tag of tags){
+    tag.addEventListener('click',tagClickHandler);
+  }
+}
+addClickListenersToTags();
